fb3017d34d0c0f66c01b9d62b215e4f9081ed35c
cxxin <862704268@qq.com>
2020-10-19 01:20:18 +0800

    完成配置

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,11 @@
 </template>
 
 <script>
-import axios from "axios"; // eslint-disable-line no-unused-vars
 export default {
-  name: "App",
-  components: {},
-  data() {
-    return {
-      res: {},
-    };
-  },
-  mounted() {
-    this.axios.get("/mock/user/login.json").then((res) => {
-      this.res = res;
-    });
-  },
+  name: "app",
 };
 </script>
+
 
 <style>
 </style>
